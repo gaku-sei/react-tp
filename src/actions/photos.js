351e0f7c88880bc86b5acc402c6ae7e560f6d734
Kevin Combriat <japan.combriat.kevin@gmail.com>
2016-10-14 13:52:27 +0200

    async await

--- src/actions/photos.js
+++ src/actions/photos.js
@@ -16,30 +16,39 @@ export const CREATE_PHOTO = 'CREATE_PHOTO';
 export const SUCCESS_CREATE_PHOTO = 'SUCCESS_CREATE_PHOTO';
 export const FAIL_CREATE_PHOTO = 'FAIL_CREATE_PHOTO';
 
-export const all = ({ albumId }: PhotoQuery = {}) => (dispatch: Dispatch) =>
-  fetch(`${config.url}/photos?_limit=${config.defaultLimit}${albumId ? `&albumId=${albumId}` : ''}`)
-    .then((res) => res.json())
-    .then((photos) => dispatch({ photos, type: SUCCESS_FETCH_PHOTOS }))
-    .catch((error) => dispatch({ error, type: FAIL_FETCH_PHOTOS }));
+export const all = ({ albumId }: PhotoQuery = {}) => async (dispatch: Dispatch) => {
+  try {
+    const res = await fetch(`${config.url}/photos?_limit=${config.defaultLimit}${albumId ? `&albumId=${albumId}` : ''}`);
+    return dispatch({ photos: await res.json(), type: SUCCESS_FETCH_PHOTOS });
+  } catch (error) {
+    return dispatch({ error, type: FAIL_FETCH_PHOTOS });
+  }
+};
 
-export const find = (id: string) => (dispatch: Dispatch) =>
-  fetch(`${config.url}/photos/${id}`)
-    .then((res) => res.json())
-    .then((photo) => dispatch({ photo, type: SUCCESS_FETCH_PHOTO }))
-    .catch((error) => dispatch({ error, type: FAIL_FETCH_PHOTO }));
+export const find = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    const res = await fetch(`${config.url}/photos/${id}`);
+    return dispatch({ photo: await res.json(), type: SUCCESS_FETCH_PHOTO });
+  } catch (error) {
+    return dispatch({ error, type: FAIL_FETCH_PHOTO });
+  }
+};
 
-export const create = (photo: Photo) => (dispatch: Dispatch) =>
-  fetch(`${config.url}/photos`, {
-    body: JSON.stringify(photo),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    method: 'POST',
-  })
-    .then((res) => res.json())
-    .then((photo) => dispatch({ photo, type: SUCCESS_CREATE_PHOTO }))
-    .catch((error) => dispatch({ error, type: FAIL_CREATE_PHOTO }));
+export const create = (photo: Photo) => async (dispatch: Dispatch) => {
+  try {
+    const res = await fetch(`${config.url}/photos`, {
+      body: JSON.stringify(photo),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+    return dispatch({ photo: await res.json(), type: SUCCESS_CREATE_PHOTO });
+  } catch (error) {
+    return dispatch({ error, type: FAIL_CREATE_PHOTO });
+  }
+};
 
 export const reinit = () => ({
   photo: {},
